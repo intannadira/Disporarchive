2ee7d84314c7d4d1d7801187b64e4a93a083cec0
muhalimashar21 <39429176+muhalimashar21@users.noreply.github.com>
2023-05-07 12:17:34 +0700

    clean-file-sampah

--- routes/web.php
+++ routes/web.php
@@ -4,14 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
 
-use App\Http\Controllers\Admin\GasController;
 use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\Supir\ScanController;
-use App\Http\Controllers\Admin\SopirController;
-use App\Http\Controllers\Admin\RelasiController;
 
-use App\Http\Controllers\Admin\LaporanController;
-use App\Http\Controllers\Admin\TipeGasController;
 use App\Http\Controllers\User1\HomeUser1Controller;
 use App\Http\Controllers\User2\HomeUser2Controller;
 use App\Http\Controllers\User3\HomeUser3Controller;
@@ -21,34 +15,10 @@ use App\Http\Controllers\User6\HomeUser6Controller;
 use App\Http\Controllers\User7\HomeUser7Controller;
 use App\Http\Controllers\User8\HomeUser8Controller;
 use App\Http\Controllers\User9\HomeUser9Controller;
-use App\Http\Controllers\Admin1\HomeAdmin1Controller;
-use App\Http\Controllers\Admin2\HomeAdmin2Controller;
-use App\Http\Controllers\Admin3\HomeAdmin3Controller;
 use App\Http\Controllers\User10\HomeUser10Controller;
 use App\Http\Controllers\User11\HomeUser11Controller;
 use App\Http\Controllers\User12\HomeUser12Controller;
-use App\Http\Controllers\Admin\SearchBarcodeController;
-use App\Http\Controllers\SuperAdmin\HakAksesController;
-use App\Http\Controllers\SuperAdmin\KaryawanController;
-use App\Http\Controllers\SuperAdmin\SuratMasukController;
-use App\Http\Controllers\User2\SuratMasukUser2Controller;
-use App\Http\Controllers\User3\SuratMasukUser3Controller;
-use App\Http\Controllers\User4\SuratMasukUser4Controller;
-use App\Http\Controllers\User5\SuratMasukUser5Controller;
-use App\Http\Controllers\User6\SuratMasukUser6Controller;
-use App\Http\Controllers\User7\SuratMasukUser7Controller;
-use App\Http\Controllers\User8\SuratMasukUser8Controller;
-use App\Http\Controllers\User9\SuratMasukUser9Controller;
-use App\Http\Controllers\SuperAdmin\SuratKeluarController;
-use App\Http\Controllers\Admin1\SuratMasukAdmin1Controller;
-use App\Http\Controllers\Admin2\SuratMasukAdmin2Controller;
-use App\Http\Controllers\Admin3\SuratMasukAdmin3Controller;
-use App\Http\Controllers\SuperAdmin\HistoriSuratController;
-use App\Http\Controllers\User10\SuratMasukUser10Controller;
-use App\Http\Controllers\User11\SuratMasukUser11Controller;
-use App\Http\Controllers\User12\SuratMasukUser12Controller;
 use App\Http\Controllers\User1\HistoriSuratUser1Controller;
-use App\Http\Controllers\SuperAdmin\JabatanBidangController;
 use App\Http\Controllers\User1\DisposisiSayaUser1Controller;
 use App\Http\Controllers\User2\DisposisiSayaUser2Controller;
 use App\Http\Controllers\User3\DisposisiSayaUser3Controller;
@@ -58,15 +28,42 @@ use App\Http\Controllers\User6\DisposisiSayaUser6Controller;
 use App\Http\Controllers\User7\DisposisiSayaUser7Controller;
 use App\Http\Controllers\User8\DisposisiSayaUser8Controller;
 use App\Http\Controllers\User9\DisposisiSayaUser9Controller;
+use App\Http\Controllers\User10\DisposisiSayaUser10Controller;
+use App\Http\Controllers\User11\DisposisiSayaUser11Controller;
+
+use App\Http\Controllers\User2\SuratMasukUser2Controller;
+use App\Http\Controllers\User3\SuratMasukUser3Controller;
+use App\Http\Controllers\User4\SuratMasukUser4Controller;
+use App\Http\Controllers\User5\SuratMasukUser5Controller;
+use App\Http\Controllers\User6\SuratMasukUser6Controller;
+use App\Http\Controllers\User7\SuratMasukUser7Controller;
+use App\Http\Controllers\User8\SuratMasukUser8Controller;
+use App\Http\Controllers\User9\SuratMasukUser9Controller;
+use App\Http\Controllers\User10\SuratMasukUser10Controller;
+use App\Http\Controllers\User11\SuratMasukUser11Controller;
+use App\Http\Controllers\User12\SuratMasukUser12Controller;
+use App\Http\Controllers\Admin1\SuratMasukAdmin1Controller;
+use App\Http\Controllers\Admin2\SuratMasukAdmin2Controller;
+use App\Http\Controllers\Admin3\SuratMasukAdmin3Controller;
 use App\Http\Controllers\Admin1\HistoriSuratAdmin1Controller;
 use App\Http\Controllers\Admin2\HistoriSuratAdmin2Controller;
 use App\Http\Controllers\Admin3\HistoriSuratAdmin3Controller;
-use App\Http\Controllers\SuperAdmin\HomeSuperAdminController;
 use App\Http\Controllers\Admin1\DisposisiSayaAdmin1Controller;
 use App\Http\Controllers\Admin2\DisposisiSayaAdmin2Controller;
-use App\Http\Controllers\User10\DisposisiSayaUser10Controller;
-use App\Http\Controllers\User11\DisposisiSayaUser11Controller;
-use App\Http\Controllers\Supir\LaporanController as LaporanSupirController;
+use App\Http\Controllers\Admin1\HomeAdmin1Controller;
+use App\Http\Controllers\Admin2\HomeAdmin2Controller;
+use App\Http\Controllers\Admin3\HomeAdmin3Controller;
+
+use App\Http\Controllers\SuperAdmin\JabatanBidangController;
+use App\Http\Controllers\SuperAdmin\HomeSuperAdminController;
+use App\Http\Controllers\SuperAdmin\HistoriSuratController;
+use App\Http\Controllers\SuperAdmin\SuratKeluarController;
+use App\Http\Controllers\SuperAdmin\HakAksesController;
+use App\Http\Controllers\SuperAdmin\KaryawanController;
+use App\Http\Controllers\SuperAdmin\SuratMasukController;
+
+
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -124,36 +121,6 @@ Route::group([
         }else {
             return redirect()->route('login');
         }
-    });
-
-    Route::group([
-        'middleware' => 'role.admin'
-    ], function () {
-
-        Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
-
-            Route::resource('sopir', SopirController::class);
-            Route::resource('gas', GasController::class);
-            Route::resource('tipe-gas', TipeGasController::class);
-            Route::resource('relasi', RelasiController::class);
-            Route::resource('laporan', LaporanController::class);
-            Route::resource('dashboard', DashboardController::class);
-            Route::resource('searchbarcode', SearchBarcodeController::class);
-
-        });
-
-    });
-
-    Route::group([
-        'middleware' => 'role.supir'
-    ], function () {
-
-        Route::group(['prefix' => 'supir', 'as' => 'supir.'], function () {
-            
-            Route::resource('scan', ScanController::class);
-            Route::resource('laporan', LaporanSupirController::class);
-        });
-
     });
 
     //role superadmin
